84f01f8d72b4598d177f7f853c4a68500cc30c32
mickael.pezzoni <mickael.pezzoni@groupe-orqual.com>
2021-06-17 23:17:45 +0200

    refactor: clean web

--- web/src/app/interfaces/discord/guild-interface.ts
+++
@@ -1,8 +0,0 @@
-export interface GuildInterface {
-    features: string[];
-    icon: string | null;
-    id: string;
-    name: number;
-    owner: boolean;
-    permisions: string;
-}
--- web/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  client_id: '853620257620099072',
-  redirect_url: 'http://localhost:4200/test',
-  discord_api: 'https://discord.com/api'
-};
